96df06e57eb09874766020c0cb4f6f7b799fa6c3
Eduardo Alvarado <33001422+edualvarado@users.noreply.github.com>
2020-06-15 16:30:09 +0200

    Load MNIST

--- main.py
+++ main.py
@@ -1,13 +1,30 @@
+"""
+#####################
+Assignment - InstaLOD
+#####################
+Deep Convolutional Generative Adversarial Network (GANs)
+Author: Eduardo Alvarado
+Task: In this assignment, I will create a GAN in order to generate novel numbers based on the MNIST dataset.
+"""
+
 import tensorflow as tf
+print(tf.__version__)
 
-mnist = tf.keras.datasets.mnist
+import matplotlib.pyplot as plt
+import numpy as np
 
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
-x_train, x_test = x_train / 255.0, x_test / 255.0
 
-model = tf.keras.models.Sequential([
-  tf.keras.layers.Flatten(input_shape=(28, 28)),
-  tf.keras.layers.Dense(128, activation='relu'),
-  tf.keras.layers.Dropout(0.2),
-  tf.keras.layers.Dense(10)
-])
+# MNIST dataset - define sets and print information
+from tensorflow.keras.datasets.mnist import load_data
+(train_x, train_y), (test_x, test_y) = load_data()
+print("Train X shape: {} - Train Y shape: {}".format(train_x.shape, train_y.shape))
+print("Test X shape: {} - Test Y shape: {}".format(test_x.shape, test_y.shape))
+
+# Plotting numbers for testing
+'''
+for i in range(10):
+    plt.subplot(2,5,i+1)
+    plt.axis("off")
+    plt.imshow(train_x[i], cmap="gray_r")
+plt.show()
+'''
